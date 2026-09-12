b38453df8f7d0caf21353836dbb4d94c0a89a943
hppdpd <0688082879s@gmail.com>
2026-09-12 12:02:41 +0300

    feat: add lib module with helper functions

---
+++ lib.py
@@ -0,0 +1,17 @@
+def add_numbers(a: float, b: float) -> float:
+    """
+    Обчислює суму двох чисел.
+    :param a: Перше число
+    :param b: Друге число
+    :return: Сума чисел
+    """
+    return a + b
+
+
+def greet_user(name: str) -> str:
+    """
+    Формує вітальне повідомлення для користувача.
+    :param name: Ім'я користувача
+    :return: Рядок з привітанням
+    """
+    return f"Привіт, {name}! Модуль lib успішно імпортовано."
